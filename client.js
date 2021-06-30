0b06cd3b4155af6a2643d6ffde2ccbc5928a4e7e
Catherine Hu <catherinehu_9898@hotmail.com>
2021-06-30 20:29:45 +0000

    movement

--- client.js
+++ client.js
@@ -4,13 +4,19 @@ const connect = () => {
   host: '135.23.223.133', 
   port: 50542 
 });
-conn.setEncoding("utf8");
-conn.on('connect', () => {
-  conn.write("Name: C")
-})
-return conn;
+
+const uponConnect = () => {
+  conn.write("Name: oop")
+  console.log('Successfully connected to game server')
+
+  //conn.write("Move: up");
 }
 
+conn.setEncoding("utf8");
+
+conn.on("connect", uponConnect)
+return conn;
+}
 
 
 module.exports = connect
